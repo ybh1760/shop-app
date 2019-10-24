9d3b28a48b91fb9e370d5ad94b0319f28026c42e
Yang <ybh1760@gmail.com>
2019-10-24 14:55:15 +0900

    ProductItem 디자인

---
+++ components/shop/ProductItem.js
@@ -0,0 +1,76 @@
+import React from "react";
+import {
+  View,
+  Button,
+  Text,
+  StyleSheet,
+  Image,
+  Dimensions,
+  Platform
+} from "react-native";
+import Colors from "../../constants/Colors";
+
+const { width, height } = Dimensions.get("window");
+
+const ProductItem = props => {
+  const { image, title, price, onViewDetail, onToCart } = props;
+  return (
+    <View style={styles.product}>
+      <View style={styles.imageContainer}>
+        <Image style={styles.image} source={{ uri: image }} />
+      </View>
+      <View style={styles.content}>
+        <Text style={styles.title}>{title}</Text>
+        <Text style={styles.price}>${price}</Text>
+      </View>
+      <View style={styles.actions}>
+        <Button
+          color={Colors.primary}
+          title="View Detail"
+          onPress={onViewDetail}
+        />
+        <Button color={Colors.primary} title="To Cart" onPress={onToCart} />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  product: {
+    marginVertical: 10,
+    borderRadius: 10,
+    shadowColor: "black",
+    shadowOpacity: 0.26,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 8,
+    elevation: 7,
+    width: "90%",
+    height: Platform.OS === "android" ? height * 0.4 : height * 0.33,
+    backgroundColor: "white",
+    alignSelf: "center"
+  },
+  imageContainer: {
+    borderTopRightRadius: 10,
+    borderTopLeftRadius: 10,
+    overflow: "hidden",
+    width: "100%",
+    height: "60%"
+  },
+  image: { width: "100%", height: "100%" },
+  content: {
+    paddingTop: Platform.OS === "android" ? height * 0.01 : height * 0.02,
+    alignItems: "center",
+    height: "15%"
+  },
+  title: { fontSize: 16 },
+  price: { fontSize: 14, color: "#888" },
+  actions: {
+    paddingHorizontal: 20,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    height: "25%"
+  }
+});
+
+export default ProductItem;
