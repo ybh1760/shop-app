29d4f09f6d4ecda8f003bee55ba9e4c2203a586c
Yang <ybh1760@gmail.com>
2019-10-25 19:13:03 +0900

    UserProducts스크린 생성

--- navigation/ShopNavigator.js
+++ navigation/ShopNavigator.js
@@ -10,6 +10,7 @@ import CartScreen from "../screens/shop/CartScreen";
 import OrdersScreen from "../screens/shop/OrdersScreen";
 import Colors from "../constants/Colors";
 import { Ionicons } from "@expo/vector-icons";
+import UserProductsScreen from "../screens/user/UserProductsScreen";
 
 const defaultNavOptions = {
   headerStyle: {
@@ -58,10 +59,29 @@ const OrdersNavigator = createStackNavigator(
   }
 );
 
+const UserNavigator = createStackNavigator(
+  {
+    userProduct: UserProductsScreen
+  },
+  {
+    navigationOptions: {
+      drawerIcon: drawerConfig => (
+        <Ionicons
+          name={Platform.OS === "android" ? "md-create" : "ios-create"}
+          size={23}
+          color={drawerConfig.tintColor}
+        />
+      )
+    },
+    defaultNavigationOptions: defaultNavOptions
+  }
+);
+
 const ShopNavigator = createDrawerNavigator(
   {
     Products: ProductNavigator,
-    Orders: OrdersNavigator
+    Orders: OrdersNavigator,
+    User: UserNavigator
   },
   {
     contentOptions: {
