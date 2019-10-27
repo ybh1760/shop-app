ff101f28e4df9959eb4297f13a7d3e9535eaf4d2
Yang <ybh1760@gmail.com>
2019-10-27 22:06:07 +0900

    Implementing a Basic Login Screen

--- navigation/ShopNavigator.js
+++ navigation/ShopNavigator.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Platform } from "react-native";
-import { createAppContainer } from "react-navigation";
+import { createAppContainer, createSwitchNavigator } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 import { createDrawerNavigator } from "react-navigation-drawer";
 
@@ -12,6 +12,7 @@ import Colors from "../constants/Colors";
 import { Ionicons } from "@expo/vector-icons";
 import UserProductsScreen from "../screens/user/UserProductsScreen";
 import EditProductScreen from "../screens/user/EditProductScreen";
+import AuthScreen from "../screens/user/AuthScreen";
 
 const defaultNavOptions = {
   headerStyle: {
@@ -92,4 +93,16 @@ const ShopNavigator = createDrawerNavigator(
   }
 );
 
-export default createAppContainer(ShopNavigator);
+const AuthNavigator = createStackNavigator({
+  Auth: AuthScreen
+},{
+  
+  defaultNavigationOptions: defaultNavOptions
+})
+
+const MainNavigator = createSwitchNavigator({
+  Auth: AuthNavigator,
+  Shop:ShopNavigator
+})
+
+export default createAppContainer(MainNavigator);
